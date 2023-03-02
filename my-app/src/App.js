c15f71716b914589280655f602a366bb370b54d9
ayedibtissem <116677898+ayedibtissem@users.noreply.github.com>
2023-03-02 08:30:53 +0100

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+
+
+import "./App.css";
+
+
+import QuestionList from "./components/QuestionList";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+
+   
+   <h1>E-LEARNING QUIZ</h1>   
+      <QuestionList>  
+    </QuestionList>
+       </div>
+     );
+   }
+   export default App;
+
+
---
+++ my-app/src/components/Question.js
@@ -0,0 +1,39 @@
+
+import React from "react";
+
+
+function Question({question}) {
+ 
+  
+  
+  return (
+    <div>
+
+  <p>{question.title}</p>
+  <p>{question.text}</p>
+
+  <div id="options">
+  {question.options.map((option, index) => (
+    <div key={index}>
+      <input type="radio" id={`option${index}`} name="options" value={option} />
+      <label htmlFor={`option${index}`}>{option}</label>
+    </div>
+  ))}
+ 
+  
+      <button type="submit" >Submit</button>
+</div>
+    </div>
+  );
+}
+
+export default Question;
+
+
+
+      
+ 
+
+
+   
+ 
